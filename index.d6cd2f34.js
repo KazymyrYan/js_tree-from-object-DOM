92cd6e4c5c557202dc6463305694cac322d69e21
Yan Kazymyr <kazymyr.yan@gmail.com>
2025-08-11 22:59:38 +0300

    make build

---
+++ index.d6cd2f34.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in n.appendChild(r),t){var l=document.createElement("li");l.textContent=a,r.appendChild(l),"object"==typeof t[a]&&null!==t[a]&&Object.keys(t[a]).length>0&&e(l,t[a])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.d6cd2f34.js.map
